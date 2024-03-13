c4f60a0a61708951de5f4ce90e14098dc681d6ab
jorgeiras <jorgeiras.alonso@hotmail.com>
2024-03-13 16:22:39 +0100

    added OS variables

--- buscainstrumentos_back/settings.py
+++ buscainstrumentos_back/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-np!klki!nv=9ew2z9qfkxa%teir-&y@#_kl$eg(sasize5z!6g'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['192.168.1.131', 'localhost' ,  '127.0.0.1']
+ALLOWED_HOSTS = [os.getenv('DROPLET_IP')]
 
 
 # Application definition
@@ -82,11 +83,11 @@ WSGI_APPLICATION = 'buscainstrumentos_back.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'buscainstrumentos_db',
-        'USER':'postgres',
-        'PASSWORD':'admin',
-        'HOST':'localhost',
-        'PORT':'5432'
+        'NAME': os.environ.get('DB_NAME'),
+        'USER':os.environ.get('DB_USER'),
+        'PASSWORD':os.environ.get('DB_PASS'),
+        'HOST':os.environ.get('DB_HOST'),
+        'PORT':os.environ.get('DB_PORT')
     }
 }
 
