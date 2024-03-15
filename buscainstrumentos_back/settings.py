5dfab60cce08189df307b8c2faee5f034e2fab3c
jorgeiras <jorgeiras.alonso@hotmail.com>
2024-03-15 13:05:57 +0100

    added static files config

--- buscainstrumentos_back/settings.py
+++ buscainstrumentos_back/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [os.getenv('DROPLET_IP')]
+ALLOWED_HOSTS = [os.getenv('DROPLET_IP'), 'localhost']
 
 
 # Application definition
@@ -132,3 +132,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
