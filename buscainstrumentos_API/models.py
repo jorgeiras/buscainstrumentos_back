02bb5718a33ee2fa32b64a0caa45dc96cddc7a91
jorgeiras <jorgeiras.alonso@hotmail.com>
2024-03-29 13:03:57 +0100

    change in the limit of the price

--- buscainstrumentos_API/models.py
+++ buscainstrumentos_API/models.py
@@ -6,7 +6,7 @@ from django.db import models
 class Instrument(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=500)
-    price = models.DecimalField(max_digits=6, decimal_places=2, null=True, default=None)
+    price = models.DecimalField(max_digits=10, decimal_places=2, null=True, default=None)
     link = models.CharField(max_length=1000, unique=True)
     website = models.CharField(max_length=500)
     image = models.CharField(max_length=1000, null=True, default=None)
