4d9ed0bf61a37c701266040d3ce1956a35a03240
jorgeiras <jorgeiras.alonso@hotmail.com>
2024-04-01 02:36:18 +0200

    change pagination to 50 per page

--- buscainstrumentos_back/settings.py
+++ buscainstrumentos_back/settings.py
@@ -137,5 +137,5 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10  # Specify the number of items per page
+    'PAGE_SIZE': 50  # Specify the number of items per page
 }
