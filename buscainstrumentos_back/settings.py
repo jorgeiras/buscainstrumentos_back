2f20621b9a6f85a59057ea592e949069aa92a2ea
jorgeiras <jorgeiras.alonso@hotmail.com>
2024-06-27 14:40:26 +0200

    added backend domain allowance

--- buscainstrumentos_back/settings.py
+++ buscainstrumentos_back/settings.py
@@ -26,7 +26,11 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [os.getenv('DROPLET_IP'),'127.0.0.1', 'localhost']
+ALLOWED_HOSTS = [os.getenv('DROPLET_IP'),'127.0.0.1', 'localhost', "https://jorgeiras.github.io/buscainstrumentos_front", 'buscainstrumentosback.duckdns.org']
+
+CORS_ALLOWED_ORIGINS = [
+    "https://jorgeiras.github.io/buscainstrumentos_front",
+]
 
 
 # Application definition
